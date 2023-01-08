e30bdb054caa50fdc60c83ae835b429919e4e84e
Tamandeep-Singh <tamandeep.singh.business@gmail.com>
2023-01-08 18:12:07 +0000

    added CI pipeline for github actions

--- config/app.config.js
+++ config/app.config.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 
 module.exports = {
-    SERVER_PORT: process.env.SERVER_PORT,
-    JEST_PORT: process.env.JEST_PORT
+    SERVER_PORT: process.env.SERVER_PORT
 }
--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -1,15 +1,5 @@
 const request = require('supertest');
 const app = require('../src/app');
-const config = require('../config/app.config');
-const JEST_PORT = config.JEST_PORT || 5094;
-
-beforeAll(() => {
-    server = app.listen(JEST_PORT); 
-});
- 
- afterAll(() => {
-    server.close();
-});
 
 describe("Test Dummy endpoint", () => {
      it("Should return status 200 and send back a simple JSON response with a timestamp", async () => {
